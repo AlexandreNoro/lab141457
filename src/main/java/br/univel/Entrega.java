f954b2ec55da71185fb94fb81029b27aafb870f2
Norinho <xando10_noro@hotmail.com>
2016-08-14 23:01:44 -0300

    #3 Criado as classes

---
+++ src/main/java/br/univel/Entrega.java
@@ -0,0 +1,82 @@
+package br.univel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Entrega implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 3822326798234322557L;
+
+	private int id_entrega;
+	private String endereco;
+	private boolean entrega;
+	private String data_venda;
+	private String data_entrega;
+	private List<String> itens;
+	private BigDecimal vlrTotal;
+
+	public Entrega() {
+
+	}
+
+	public int getId_entrega() {
+		return id_entrega;
+	}
+
+	public void setId_entrega(int id_entrega) {
+		this.id_entrega = id_entrega;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public boolean isEntrega() {
+		return entrega;
+	}
+
+	public void setEntrega(boolean entrega) {
+		this.entrega = entrega;
+	}
+
+	public String getData_venda() {
+		return data_venda;
+	}
+
+	public void setData_venda(String data_venda) {
+		this.data_venda = data_venda;
+	}
+
+	public String getData_entrega() {
+		return data_entrega;
+	}
+
+	public void setData_entrega(String data_entrega) {
+		this.data_entrega = data_entrega;
+	}
+
+	public List<String> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<String> itens) {
+		this.itens = itens;
+	}
+
+	public BigDecimal getVlrTotal() {
+		return vlrTotal;
+	}
+
+	public void setVlrTotal(BigDecimal vlrTotal) {
+		this.vlrTotal = vlrTotal;
+	}
+
+}
---
+++ src/main/java/br/univel/Venda.java
@@ -0,0 +1,48 @@
+package br.univel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Venda implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -8052670432604566361L;
+
+	private int id_venda;
+	private String cpf_cliente;
+	private List<String> itens;
+	private BigDecimal vlrTotal;
+
+
+	public int getId_venda() {
+		return id_venda;
+	}
+	public void setId_venda(int id_venda) {
+		this.id_venda = id_venda;
+	}
+	public String getCpf_cliente() {
+		return cpf_cliente;
+	}
+	public void setCpf_cliente(String cpf_cliente) {
+		this.cpf_cliente = cpf_cliente;
+	}
+	public List<String> getItens() {
+		return itens;
+	}
+	public void setItens(List<String> itens) {
+		this.itens = itens;
+	}
+	public BigDecimal getVlrTotal() {
+		return vlrTotal;
+	}
+	public void setVlrTotal(BigDecimal vlrTotal) {
+		this.vlrTotal = vlrTotal;
+	}
+
+
+
+
+}
