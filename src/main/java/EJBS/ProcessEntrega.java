af912f4e358aff221a7f274cc189a50161905a4d
Norinho <xando10_noro@hotmail.com>
2016-08-14 23:18:03 -0300

    #4 Criado os EJBS

---
+++ src/main/java/EJBS/ProcessEntrega.java
@@ -0,0 +1,13 @@
+package EJBS;
+
+import javax.ejb.Stateless;
+
+import br.univel.Entrega;
+
+@Stateless
+public class ProcessEntrega {
+
+	public void processarEntrega(Entrega entrega) {
+
+	}
+}
---
+++ src/main/java/EJBS/ProcessVenda.java
@@ -0,0 +1,13 @@
+package EJBS;
+
+import javax.ejb.Stateless;
+
+import br.univel.Venda;
+
+@Stateless
+public class ProcessVenda {
+
+	public void processarVenda(Venda venda) {
+
+	}
+}
