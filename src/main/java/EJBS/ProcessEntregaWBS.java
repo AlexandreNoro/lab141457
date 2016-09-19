0a5be665bf4e975a965897aca8220206c43a0e8b
Norinho <xando10_noro@hotmail.com>
2016-09-19 00:49:57 -0300

    Criado interface de Processo de entrega e venda  para as classes dos Webservices

---
+++ src/main/java/EJBS/ProcessEntregaWBS.java
@@ -0,0 +1,9 @@
+package EJBS;
+
+import br.univel.Entrega;
+
+public interface ProcessEntregaWBS {
+
+	public void processarEntrega(Entrega entrega);
+
+}
---
+++ src/main/java/EJBS/ProcessVendaWBS.java
@@ -0,0 +1,8 @@
+package EJBS;
+
+import br.univel.Venda;
+
+public interface ProcessVendaWBS {
+
+	public void processarVenda(Venda venda);
+}
