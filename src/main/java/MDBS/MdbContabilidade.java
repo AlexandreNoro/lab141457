e376f1fccad135359081c458e64772fc32c446ae
Norinho <xando10_noro@hotmail.com>
2016-09-20 15:10:07 -0300

    #10

--- src/main/java/MDBS/MdbContabilidade.java
+++ src/main/java/MDBS/MdbContabilidade.java
@@ -1,32 +1,54 @@
 package MDBS;
 
-import java.util.logging.Logger;
+import java.sql.Timestamp;
+import java.util.Calendar;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+
+import org.hibernate.Session;
+
+import br.univel.Log;
+import br.univel.Venda;
+import br.univel.Hibernate.ConfigHibernate;
 
 @MessageDriven(name = "MdbContabilidade", activationConfig = {
 		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/TOPICVenda"),
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
 		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
 
+@TransactionManagement(TransactionManagementType.BEAN)
 public class MdbContabilidade implements MessageListener {
-
-	private final static Logger LOGGER = Logger.getLogger(MdbContabilidade.class.toString());
 
 	@Override
 	public void onMessage(Message arg0) {
 
-		ObjectMessage omsg = null;
-		try {
-			omsg = (ObjectMessage) arg0;
-			LOGGER.warning(omsg.getObject().toString());
-			LOGGER.warning("MdbContabilidade");
-		} catch (JMSException e) {
-			e.printStackTrace();
+		if (arg0 instanceof ObjectMessage) {
+			ObjectMessage om = (ObjectMessage) arg0;
+			try {
+				Venda venda = (Venda) om.getObject();
+
+				Log logg = new Log();
+				logg.setData_time(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+				;
+				logg.setNome_mdb("MdbContabilidade");
+				logg.setMsg(venda.toString());
+
+				Session session = ConfigHibernate.getSessionFactory().openSession();
+				session.beginTransaction();
+				session.persist(logg);
+				session.getTransaction().commit();
+				session.close();
+
+			} catch (JMSException e) {
+
+				e.printStackTrace();
+			}
 		}
 	}
 
--- src/main/java/MDBS/MdbLogistica.java
+++ src/main/java/MDBS/MdbLogistica.java
@@ -1,35 +1,57 @@
 package MDBS;
 
-import java.util.logging.Logger;
+import java.sql.Timestamp;
+import java.util.Calendar;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.ObjectMessage;
+import org.hibernate.Session;
+
+import br.univel.Log;
+import br.univel.Hibernate.ConfigHibernate;
 
 @MessageDriven(name = "MdbLogistica", activationConfig = {
 		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "Queue/QUEUEPedido"),
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
 		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
 
+@TransactionManagement(TransactionManagementType.BEAN)
 public class MdbLogistica implements MessageListener {
-
-	private final static Logger LOGGER = Logger.getLogger(MdbLogistica.class.toString());
 
 	@Override
 	public void onMessage(Message arg0) {
-		ObjectMessage omsg = null;
-		try {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Log logg1 = new Log();
+				logg1.setData_time(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+				logg1.setNome_mdb("MdbLogistica");
+				logg1.setMsg("Iniciando envio");
 
-			LOGGER.warning("Despachando pedido......");
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
+				Session session = ConfigHibernate.getSessionFactory().openSession();
+				session.beginTransaction();
+				session.persist(logg1);
 
-			e.printStackTrace();
-		}
-		LOGGER.warning("Entrega Despachada!");
+				try {
+					Thread.sleep(20000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
+				Log logg2 = new Log();
+				logg2.setData_time(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+				logg2.setNome_mdb("MdbLogistica");
+				logg2.setMsg("Entrega enviada!");
+
+				session.persist(logg2);
+				session.getTransaction().commit();
+				session.close();
+			}
+		}).start();
 	}
 
 }
